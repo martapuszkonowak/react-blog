8b5fc44618f28cd0d61d64e138a529d1558dfb3c
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-16 14:21:44 +0100

    Updated css, created onClick event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("Hello");
+  };
+
   return (
     <div className="home">
-      <h2>Homepage</h2>;
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
