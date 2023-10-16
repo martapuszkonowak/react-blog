dbfd5b586791c480331680c4dcaaa3aec5146fe5
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-16 14:33:47 +0100

    Removed console.log

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Marta");
+
   const handleClick = () => {
-    console.log("Hello");
+    setName("Koko");
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
