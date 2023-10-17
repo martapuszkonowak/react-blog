157becb6f144de53fce7cd7e81a2fb5ff9273675
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-17 10:31:11 +0100

    Added css

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Marta");
-
-  const handleClick = () => {
-    setName("Koko");
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author} </p>
+        </div>
+      ))}
     </div>
   );
 };
