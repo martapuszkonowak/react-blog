59cff053698d241cf8918d1a7b0eb17fd3db1807
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-17 11:09:56 +0100

    Added new component BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+    { title: "My new website", body: "lorem ipsum...", author: "Marta", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "Nina", id: 2 },
     {
       title: "Web dev top tips",
       body: "lorem ipsum...",
-      author: "Mario",
+      author: "Oliver",
       id: 3,
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+  });
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author} </p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
     </div>
   );
 };
