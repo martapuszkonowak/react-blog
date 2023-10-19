1c87ccd04bc2b9d9e879ce86954c82b6a984d7fc
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-19 11:10:06 +0100

    Linked to blogs' id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+
 function App() {
   return (
     <Router>
@@ -14,6 +16,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
