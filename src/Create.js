a614654f13048ee8be481b2787ce81e2b3620bcd
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-23 16:49:33 +0100

    Added form and css

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,18 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required />
+        <label>Blog body:</label>
+        <textarea required></textarea>
+        <label>Blog author</label>
+        <select>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
